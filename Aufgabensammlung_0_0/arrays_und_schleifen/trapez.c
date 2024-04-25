c4265378887fe1b1031ea964fc7eecfd5db0e495
ANAS BUCK <160623167+maitlahcen2645@users.noreply.github.com>
2024-04-25 14:26:06 +0200

    Trapez

--- Aufgabensammlung_0_0/arrays_und_schleifen/trapez.c
+++ Aufgabensammlung_0_0/arrays_und_schleifen/trapez.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-// Wurde erstellt am 24.04.2024 :)
-int main()
-{
-    int i = 0;
-    int f = 0;
 
-    for(i = 0; i < 7; i++)
+// Wurde erstellt am 24.04.2024 :)
+
+void mach_leer(int a)
+{
+    for (int i = 0; i < a; i++)
     {
-        for(f = -3; f < i; f++)
-        {
-            printf("*");
-        }
+        printf(" ");
+    }
+}
+
+void mach_stern(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        printf("*");
+    }
+}
+
+int main()
+{   
+    int zeilen = 5;
+    for (int i = 0; i < zeilen; i++)
+    {
+        mach_leer(zeilen - i - 1);
+        mach_stern(2 * i + 1);
         printf("\n");
     }
-    
+    return 0;
 }
